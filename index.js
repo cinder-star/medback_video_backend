1db6e798cb7151ff55fcddd5daf88195bf8672cc
sihan-tawsik <sihantawsik@gmail.com>
2020-08-25 01:20:05 +0600

    added heroku procfile

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ app.post('/video/token', (req, res) => {
   sendTokenResponse(token, res);
 });
 
-app.listen(3001, () =>
+app.listen(process.env.PORT || 3001, () =>
   console.log('Express server is running on localhost:3001')
 );
