15d3b2b178678433bf890c2fde33601f05eaa645
sihan-tawsik <sihantawsik@gmail.com>
2020-08-25 15:18:14 +0600

    added cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const config = require('./server/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { videoToken } = require('./server/tokens');
+const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
